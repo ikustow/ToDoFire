11741c12458957a229ec128d3b8ab1c98657918d
ikustow <ikustow@yandex.run>
2020-10-10 16:57:16 +0300

    Initial Commit

---
+++ ToDoFire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoFire
+//
+//  Created by Ilya on 10.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
